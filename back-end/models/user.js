f2ae60936cfd059b57532dcb375be268f7c608aa
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-06-18 11:15:44 +0200

    //MongoDB "user" schema creation

---
+++ back-end/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//MongoDB schema creation 
+const userSchema = new Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+});
+
+
+module.exports = mongoose.model('User', userSchema);
