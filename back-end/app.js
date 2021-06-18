e1513b6ba16485bcc31c76a941ad9f0fd123774e
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-06-18 18:11:51 +0200

    first user creation succesful

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+
 const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/user');
 
+const app = express();
 //MongoDB Atlas connection
 mongoose.connect('mongodb+srv://new-user01:12345@cluster0.jw3cx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -15,11 +16,11 @@ mongoose.connect('mongodb+srv://new-user01:12345@cluster0.jw3cx.mongodb.net/myFi
 
 // Cross Origin Resource Sharing
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.use(bodyParser.json());
 
