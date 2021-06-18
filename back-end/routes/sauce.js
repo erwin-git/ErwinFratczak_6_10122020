5c486689fccac37a2f647f1d4b613a12fbe0d40c
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-06-18 22:22:18 +0200

    sauce routes creation

---
+++ back-end/routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const saucesCtrl = require('../controllers/sauces');
+
+const auth = require('../middleware/auth');
+
+
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+
+module.exports = router;
