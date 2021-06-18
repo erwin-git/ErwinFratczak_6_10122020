f96dca8893f24eb4881160a00652ba66ae4e95db
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-06-18 22:10:49 +0200

    sauce GET route

--- back-end/routes/sauce.js
+++ back-end/routes/sauce.js
@@ -12,3 +12,9 @@ exports.createSauce = (req, res, next) => {
         .then(() => res.status(201).json({message: 'Sauce enregistrée !'}))
     .catch((error) => res.status(400).json({ error }));
 };
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }))
+};
