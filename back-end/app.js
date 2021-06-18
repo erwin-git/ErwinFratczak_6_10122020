7375f298de032b0546ea330d166c042abf43a23c
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-06-18 16:02:08 +0200

    user contromers init

--- back-end/app.js
+++ back-end/app.js
@@ -21,34 +21,15 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-console.log(req.body);
-res.status(201).json({
-    message: 'Objet créé !'
-});
-});
-
-
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-        ];
-        res.status(200).json(stuff);
+app.post('/api/auth/signup', (req, res) => {
+    console.log(req.body);
+    res.status(201).end('ok')
+    res.status(201).json({
+        message: "utilisateur inscrit"
     });
+});
+
+
+
 
 module.exports = app;
