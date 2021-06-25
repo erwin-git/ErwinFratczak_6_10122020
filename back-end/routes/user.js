8b89a5a210232f5026ecba0c2afc596d1b993778
Erwin Fratczak <erwin.fratczak@gmail.com>
2021-06-25 11:47:07 +0200

    Express rate limit install

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const limiter = require('../middleware/express-rate-limit-config');
 const userCtrl = require('../controllers/user');
+
 //Routes creation for users
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', limiter, userCtrl.login);
 
 module.exports = router;
